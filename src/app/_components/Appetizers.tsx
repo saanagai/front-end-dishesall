1fc60d77fd5359264d4c0e2d1e26746e6cfa2678
24LP0542 24LP0542 <24LP0542@G304-CC0227.local>
2025-03-11 12:34:10 +0800

    add+

--- src/app/_components/Appetizers.tsx
+++ src/app/_components/Appetizers.tsx
@@ -1,30 +1,126 @@
-export default function () {
-  const data = [
-    {
-      name: "Brie Crostini Appertizer",
-      img: "res.clod",
-      price: "$12.99",
-      description:
-        "Fluffy pancakes stacked with fruits, cream, syrup, and powdere sugar.",
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { CloudinaryUpload } from "./CloudinaryUpload";
+import { Textarea } from "@/components/ui/textarea";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+export default function page() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
     },
-    {
-      name: "Budaatai huurga",
-      img: "res.clod",
-      price: "15000",
-      description: "Uhriin mahtai budaatai huurga",
-    },
-    {
-      name: "Budaatai huurga",
-      img: "res.clod",
-      price: "15000",
-      description: "Uhriin mahtai budaatai huurga",
-    },
-  ];
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div>
-      {data.map((hool) => {
-        return <h1>{hool.name}</h1>;
-      })}
+      <Dialog>
+        <DialogTrigger>
+          <div className="border-dashed border-2 border-red-500  w-60 h-52 rounded-lg flex flex-col justify-center items-center gap-4">
+            <div className="rounded-full size-10 bg-red-500 flex justify-center items-center text-white ">
+              +
+            </div>
+            <p className="w-32">Add new Dish to Appetizers</p>
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Add new Dish to Appetizers</DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div className="flex gap-8">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Food name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Type food name" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Food price</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter price..." {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <Textarea placeholder="List ingredients..." />
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <CloudinaryUpload />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
